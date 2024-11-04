d7fe27f88763f21ec4c1be3b825ee97c1c46096b
Raven Hendricks <ravenkbeats@gmail.com>
2024-11-04 12:34:30 -0500

    first iteration

---
+++ leap.py
@@ -0,0 +1,9 @@
+def leap_year(year):
+    if year % 4 == 0 and year % 100 == 0 and year % 400 == 0:
+        return True
+    if year % 4 == 0 and not year % 100 == 0:
+        return True
+    if not year % 4 == 0:
+        return False
+    if year % 100 == 0 and not year % 400 == 0:
+        return False
